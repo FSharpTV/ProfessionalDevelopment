c890c4a5a263e9e106efc5783994b9e3862d4464
Mark Gray <markgray.online@googlemail.com>
2016-02-18 16:09:20 +0000

    Adding code for 'Dealing a card' video

--- src/Cards/Cards.fs
+++ src/Cards/Cards.fs
@@ -17,6 +17,9 @@ type Card = Card of Suit*Rank
 
 type Deck = Card list
 
+type ShuffledDeck = ShuffledDeck of Deck
+
+type Deal = ShuffledDeck -> ShuffledDeck * Card
 
 let allSuits = [ Clubs; Diamonds; Hearts; Spades ]
 let allRanks = [ Two; Three;Four; Five; Six; Seven; Eight; Nine; Ten; Jack; Queen; King; Ace ]
@@ -25,3 +28,10 @@ let newDeck =
     [ for suit in allSuits do
         for rank in allRanks do
             yield Card (suit, rank) ]
+
+
+let dealCard : Deal =
+    fun deck ->
+        match deck with
+        | ShuffledDeck d -> ShuffledDeck d, Card(Spades,Ace)
+
