ae351255998c0c6fbe67cd68a42969c44e9e4d3d
Mark Gray <markgray.online@googlemail.com>
2016-02-18 10:40:16 +0000

    Adding files for 'Creating a deck of Cards'

---
+++ src/Cards/Cards.fs
@@ -0,0 +1,18 @@
+﻿module FSharp.TV.Cards
+
+type Suit = 
+    | Clubs
+    | Diamonds
+    | Hearts
+    | Spades
+
+type Rank = 
+    | Two   | Three | Four  
+    | Five  | Six   | Seven 
+    | Eight | Nine  | Ten   
+    | Jack  | Queen | King  
+    | Ace
+
+type Card = Card of Suit*Rank
+
+type Deck = Card list
