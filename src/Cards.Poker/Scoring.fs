e71a9c3e66304f439debe009b34b5862a40c8917
Mark Gray <markgray.online@googlemail.com>
2016-03-24 21:15:44 +0000

    Adding code for videos: 'Dealing a hand' and 'Ordering player hands'

--- src/Cards.Poker/Scoring.fs
+++ src/Cards.Poker/Scoring.fs
@@ -1,20 +1,67 @@
 ﻿module FSharp.TV.Cards.Poker.Scoring
 
 open FSharp.TV.Cards
-  
-let dealHand player : Hand list=
-  [ 
-    [ Card (Hearts,Seven)
-      Card (Clubs,Two)
-      Card (Clubs,Eight)
-      Card (Spades,Two)
-      Card (Clubs,Three) ]
 
-    [ Card (Clubs,Ten)
-      Card (Spades,Six)
-      Card (Diamonds,Nine)
-      Card (Diamonds,King)
-      Card (Hearts,Three) ] ]
+let dealHands numOfPlayers deck : Hand list =
+  
+  let cardsToDeal = numOfPlayers * 5 
+
+  let optToCard opt = 
+    match opt with
+    | Some card -> card
+    | None -> failwith "Input was None"
+
+  let rec dealCards deck cards =
+    if cards |> List.length = cardsToDeal
+    then deck, cards |> List.rev
+    else
+      let shf, opt = dealCard deck
+      let c = optToCard opt
+      dealCards shf (c::cards)
+
+  let _, cards = dealCards deck []
+
+  let removeIndexes hand = 
+    snd hand |> List.map (fun (_,c) -> c)
+
+  let indexed = cards |> Seq.indexed |> Seq.toList
+  let grouped = 
+    indexed 
+    |> List.groupBy (fun (i,c) -> (i%numOfPlayers)+1 |> sprintf "Player %i")
+  let mapped = grouped |> List.map removeIndexes
+
+  mapped
+
+let rankScore card = 
+  match card with
+  | Card(_,Two)   -> 2
+  | Card(_,Three) -> 3
+  | Card(_,Four)  -> 4
+  | Card(_,Five)  -> 5
+  | Card(_,Six)   -> 6
+  | Card(_,Seven) -> 7
+  | Card(_,Eight) -> 8
+  | Card(_,Nine)  -> 9
+  | Card(_,Ten)   -> 10
+  | Card(_,Jack)  -> 11
+  | Card(_,Queen) -> 12
+  | Card(_,King)  -> 13
+  | Card(_,Ace)   -> 14
+
+let identifyPlayers hands =
+  hands
+  |> List.mapi (fun i h -> i%1 |> sprintf "Player %i", h)
+
+let orderCardsInEachHand hands : (string*Hand) list =
+  let sort (hand:Hand) =
+    hand
+    |> List.sortByDescending rankScore
+  [ for hand in hands ->
+    fst hand, sort (snd hand)] 
 
 let evaluate hands =
+  // identify player hand
+  // order cards in each hand
+  // order by highest hand
+  // choose highest player hand
   "Player 2"
--- src/Cards.Tests/Scoring.fs
+++ src/Cards.Tests/Scoring.fs
@@ -9,7 +9,7 @@ open FsUnit
 let ``Should evaluate Player 2 to have the winning hand`` () =
   // Arrange
   // set up dealing
-  let hands = dealHand 2
+  let hands = dealHands 2
 
   // Act
   // setup call to evaluate hands
@@ -17,3 +17,81 @@ let ``Should evaluate Player 2 to have the winning hand`` () =
 
   // Assert
   chosenWinner |> should equal "Player 2"
+
+let shuffledDeck =
+  ShuffledDeck
+    [ Card (Hearts,Seven); Card (Clubs,Ten); Card (Clubs,Two);
+      Card (Spades,Six); Card (Clubs,Eight); Card (Diamonds,Nine);
+      Card (Spades,Two); Card (Diamonds,King); Card (Clubs,Three);
+      Card (Hearts,Three); Card (Hearts,Five); Card (Diamonds,Jack);
+      Card (Clubs,Four); Card (Spades,King); Card (Hearts,Eight);
+      Card (Diamonds,Seven); Card (Diamonds,Five); Card (Hearts,Ten);
+      Card (Clubs,Seven); Card (Hearts,Six); Card (Hearts,King);
+      Card (Spades,Five); Card (Hearts,Two); Card (Hearts,Four);
+      Card (Diamonds,Two); Card (Diamonds,Three); Card (Clubs,Jack);
+      Card (Clubs,Six); Card (Diamonds,Six); Card (Spades,Queen);
+      Card (Spades,Jack); Card (Diamonds,Ace); Card (Hearts,Nine);
+      Card (Hearts,Ace); Card (Diamonds,Four); Card (Spades,Four);
+      Card (Spades,Three); Card (Clubs,Queen); Card (Hearts,Queen);
+      Card (Diamonds,Queen); Card (Spades,Seven); Card (Spades,Eight);
+      Card (Clubs,Ace); Card (Spades,Nine); Card (Hearts,Jack);
+      Card (Spades,Ten); Card (Clubs,Five); Card (Diamonds,Ten);
+      Card (Clubs,Nine); Card (Clubs,King); Card (Spades,Ace);
+      Card (Diamonds,Eight) ]
+
+[<Test>]
+let ``Should deal 5 cards to each player`` () =
+
+  // Arrange
+  let deck = shuffledDeck
+
+  // Act
+  let hands = dealHands 2 deck
+  let fstPlayerHand = hands.[0]
+  let sndPlayerHand = hands.[1]
+
+  // Assert
+  fstPlayerHand |> should haveLength 5
+  sndPlayerHand |> should haveLength 5
+
+[<Test>]
+let ``Player 2 and Player 3 should have certain cards in their hands`` () =
+
+  // Arrange
+  let deck = shuffledDeck
+
+  // Act
+  let hands = dealHands 3 deck
+  let sndPlayerHand = hands.[1]
+  let thdPlayerHand = hands.[2]
+
+  // Assert
+  sndPlayerHand |> should contain (Card (Diamonds, King))
+  thdPlayerHand |> should contain (Card (Diamonds, Jack))
+
+[<Test>]
+let ``The hand should be in a specific order`` () =
+  // Arrange
+  let expected = 
+    [ Card (Diamonds,King)
+      Card (Spades,King)
+      Card (Clubs,Ten)
+      Card (Clubs,Eight)
+      Card (Hearts,Five) ]
+  
+  let unsortedHands =
+    [ [ Card (Hearts,Seven); Card (Spades,Six); Card (Spades,Two);
+        Card (Hearts,Three); Card (Clubs,Four) ];
+      [ Card (Clubs,Ten); Card (Clubs,Eight); Card (Diamonds,King);
+        Card (Hearts,Five); Card (Spades,King) ];
+      [ Card (Clubs,Two); Card (Diamonds,Nine); Card (Clubs,Three);
+        Card (Diamonds,Jack); Card (Hearts,Eight)] ]  
+  
+  let unorderedHands = identifyPlayers unsortedHands
+
+  // Act
+  let orderedHands = orderCardsInEachHand unorderedHands
+  let actual = snd orderedHands.[1]
+
+  // Assert
+  CollectionAssert.AreEqual(expected, actual)
